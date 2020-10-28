65935cd683e04407ffe88e54711401c94e623561
javier <javier.irigoyen@pucp.pe>
2020-10-28 10:42:39 -0500

    class2

--- prog_joyeria/Class1.cs
+++ prog_joyeria/Class1.cs
@@ -1,9 +1,7 @@
 ﻿using Guna.UI.WinForms;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,16 +9,24 @@ using System.Windows.Forms;
 
 namespace prog_joyeria
 {
-    public partial class Form1 : Form
+    public class BtnMenu
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
 
-        private void btn_MouseEnter(object sender, EventArgs e)
+        private Form1 form;
+        public GunaButton btn
+        { get; set; }
+
+        public Panel btnLb
+        { get; set; }
+
+        public BtnMenu(GunaButton btn,Panel btnLb)
+           {
+            
+           }
+
+        public void btn_MouseEnter(object sender, EventArgs e)
         {
-            btnLb.BackColor=Color.FromArgb(39, 50, 170);
+            btn.BackColor = Color.FromArgb(39, 50, 170);
         }
 
         private void btn_MouseLeave(object sender, EventArgs e)
@@ -31,18 +37,14 @@ namespace prog_joyeria
         private void btnLb_MouseEnter(object sender, EventArgs e)
         {
             btnLb.BackColor = Color.FromArgb(39, 50, 170);
-            btn1.BaseColor = Color.FromArgb(12, 18,41);
+            btn.BaseColor = Color.FromArgb(12, 18, 41);
 
         }
 
         private void btnLb_MouseLeave(object sender, EventArgs e)
         {
             btnLb.BackColor = Color.FromArgb(1, 3, 19);
-            btn1.BaseColor = Color.FromArgb(1, 3, 19);
+            btn.BaseColor = Color.FromArgb(1, 3, 19);
         }
-
-        BtnMenu c = new BtnMenu(GunaButton btn1;btnLbel1);
-
-    }  
-    
+    }
 }
--- prog_joyeria/Form1.Designer.cs
+++ prog_joyeria/Form1.Designer.cs
@@ -30,22 +30,66 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLb = new System.Windows.Forms.Panel();
+            this.btn1 = new Guna.UI.WinForms.GunaButton();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.btnLb = new System.Windows.Forms.Panel();
-            this.btn = new Guna.UI.WinForms.GunaButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.gunaButton5 = new Guna.UI.WinForms.GunaButton();
+            this.btnLb1 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
+            this.btnLb.SuspendLayout();
+            this.btnLb1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(19)))));
+            this.panel1.Controls.Add(this.btnLb1);
             this.panel1.Controls.Add(this.btnLb);
-            this.panel1.Controls.Add(this.btn);
+            this.panel1.Controls.Add(this.gunaButton5);
+            this.panel1.Controls.Add(this.btn1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(159, 620);
             this.panel1.TabIndex = 0;
+            // 
+            // btnLb
+            // 
+            this.btnLb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(19)))));
+            this.btnLb.Controls.Add(this.panel3);
+            this.btnLb.Location = new System.Drawing.Point(154, 98);
+            this.btnLb.Name = "btnLb";
+            this.btnLb.Size = new System.Drawing.Size(5, 54);
+            this.btnLb.TabIndex = 1;
+            this.btnLb.MouseEnter += new System.EventHandler(this.btnLb_MouseEnter);
+            this.btnLb.MouseLeave += new System.EventHandler(this.btnLb_MouseLeave);
+            // 
+            // btn1
+            // 
+            this.btn1.AnimationHoverSpeed = 0.07F;
+            this.btn1.AnimationSpeed = 0.03F;
+            this.btn1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(19)))));
+            this.btn1.BorderColor = System.Drawing.Color.Black;
+            this.btn1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btn1.FocusedColor = System.Drawing.Color.Empty;
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.Image = ((System.Drawing.Image)(resources.GetObject("btn1.Image")));
+            this.btn1.ImageSize = new System.Drawing.Size(20, 20);
+            this.btn1.Location = new System.Drawing.Point(0, 180);
+            this.btn1.Name = "btn1";
+            this.btn1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(18)))), ((int)(((byte)(41)))));
+            this.btn1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btn1.OnHoverForeColor = System.Drawing.Color.White;
+            this.btn1.OnHoverImage = null;
+            this.btn1.OnPressedColor = System.Drawing.Color.Black;
+            this.btn1.Size = new System.Drawing.Size(156, 54);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "gunaButton5";
+            this.btn1.MouseEnter += new System.EventHandler(this.btn_MouseEnter);
+            this.btn1.MouseLeave += new System.EventHandler(this.btn_MouseLeave);
             // 
             // panel2
             // 
@@ -53,7 +97,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1176, 66);
+            this.panel2.Size = new System.Drawing.Size(1245, 66);
             this.panel2.TabIndex = 1;
             // 
             // panel5
@@ -64,48 +108,65 @@
             this.panel5.Size = new System.Drawing.Size(591, 337);
             this.panel5.TabIndex = 1;
             // 
-            // btnLb
+            // panel3
             // 
-            this.btnLb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(19)))));
-            this.btnLb.Location = new System.Drawing.Point(145, 98);
-            this.btnLb.Name = "btnLb";
-            this.btnLb.Size = new System.Drawing.Size(13, 54);
-            this.btnLb.TabIndex = 1;
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(19)))));
+            this.panel3.Location = new System.Drawing.Point(77, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(5, 54);
+            this.panel3.TabIndex = 3;
             // 
-            // btn
+            // gunaButton5
             // 
-            this.btn.AnimationHoverSpeed = 0.07F;
-            this.btn.AnimationSpeed = 0.03F;
-            this.btn.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(19)))));
-            this.btn.BorderColor = System.Drawing.Color.Black;
-            this.btn.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.btn.FocusedColor = System.Drawing.Color.Empty;
-            this.btn.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btn.ForeColor = System.Drawing.Color.White;
-            this.btn.Image = ((System.Drawing.Image)(resources.GetObject("btn.Image")));
-            this.btn.ImageSize = new System.Drawing.Size(20, 20);
-            this.btn.Location = new System.Drawing.Point(0, 98);
-            this.btn.Name = "btn";
-            this.btn.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(18)))), ((int)(((byte)(41)))));
-            this.btn.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.btn.OnHoverForeColor = System.Drawing.Color.White;
-            this.btn.OnHoverImage = null;
-            this.btn.OnPressedColor = System.Drawing.Color.Black;
-            this.btn.Size = new System.Drawing.Size(148, 54);
-            this.btn.TabIndex = 0;
-            this.btn.Text = "gunaButton5";
-            this.btn.MouseEnter += new System.EventHandler(this.btn_MouseEnter);
-            this.btn.MouseLeave += new System.EventHandler(this.btn_MouseLeave);
+            this.gunaButton5.AnimationHoverSpeed = 0.07F;
+            this.gunaButton5.AnimationSpeed = 0.03F;
+            this.gunaButton5.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(19)))));
+            this.gunaButton5.BorderColor = System.Drawing.Color.Black;
+            this.gunaButton5.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaButton5.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaButton5.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaButton5.ForeColor = System.Drawing.Color.White;
+            this.gunaButton5.Image = ((System.Drawing.Image)(resources.GetObject("gunaButton5.Image")));
+            this.gunaButton5.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaButton5.Location = new System.Drawing.Point(0, 98);
+            this.gunaButton5.Name = "gunaButton5";
+            this.gunaButton5.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(18)))), ((int)(((byte)(41)))));
+            this.gunaButton5.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaButton5.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaButton5.OnHoverImage = null;
+            this.gunaButton5.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaButton5.Size = new System.Drawing.Size(156, 54);
+            this.gunaButton5.TabIndex = 2;
+            this.gunaButton5.Text = "gunaButton5";
+            // 
+            // btnLb1
+            // 
+            this.btnLb1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(19)))));
+            this.btnLb1.Controls.Add(this.panel6);
+            this.btnLb1.Location = new System.Drawing.Point(154, 180);
+            this.btnLb1.Name = "btnLb1";
+            this.btnLb1.Size = new System.Drawing.Size(5, 54);
+            this.btnLb1.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(19)))));
+            this.panel6.Location = new System.Drawing.Point(77, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(5, 54);
+            this.panel6.TabIndex = 3;
             // 
             // Form1
             // 
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(17)))), ((int)(((byte)(38)))));
-            this.ClientSize = new System.Drawing.Size(1176, 620);
+            this.ClientSize = new System.Drawing.Size(1245, 645);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.panel1.ResumeLayout(false);
+            this.btnLb.ResumeLayout(false);
+            this.btnLb1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -133,7 +194,11 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Panel btnLb;
-        private Guna.UI.WinForms.GunaButton btn;
+        private Guna.UI.WinForms.GunaButton btn1;
+        private System.Windows.Forms.Panel btnLb1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel3;
+        private Guna.UI.WinForms.GunaButton gunaButton5;
     }
 }
 
